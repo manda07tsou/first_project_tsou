a586b7b92e7b7e3a77998131bee7ba4328b73d45
vonintsoa <rhjmanda@gmail.com>
2023-03-28 09:03:40 +0300

    commit clear

---
+++ pages/produits/_produitsHeader.php
@@ -0,0 +1,21 @@
+<div class="container fond">
+    <ul class="cat-test">
+        <li class="">toutes les categories</li>
+        <li>categorie</li>
+        <li>categorie</li>
+        <li>categorie</li>
+        <li>categorie</li>
+        <li>categorie</li>
+    </ul>
+</div>
+
+<div class="product-header container mt-20">
+        <!-- <a href="#" class="btn-primary">Toutes les categories</a> -->
+        <form action="#">
+            <input type="text" class="form-control bg" placeholder="test"/>
+            <!-- <button class="btn-primary">rechercher</button> -->
+        </form>
+        <a href="#" class="panier-link btn-primary-outlined">
+            0 Items
+        </a> 
+</div> 
